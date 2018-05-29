a0fb87d11c20f9004f9603821f36c71884364b09
Simon Mangel <simon.mangel@rwth-aachen.de>
2018-05-29 17:43:30 +0200

    .sh correction

--- mensaplan.sh
+++ mensaplan.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -Dfile.encoding=UTF-8 -classpath $DIR/out/production/Mensaplan:$DIR/jsoup-1.11.2.jar Mensaplan
+java -Dfile.encoding=UTF-8 -classpath $DIR/out/production/Mensaplan:$DIR/jsoup-1.11.2.jar Mensaplan $1
